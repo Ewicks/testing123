abcda2ec3201c9dcb8011be7a295d2a5df2eec6f
ethan <ethanowicks9@gmail.com>
2024-01-14 22:53:50 +0000

    Get richmond scraper up and running

--- richmond.py
+++ richmond.py
@@ -2,16 +2,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 import re
 import time
 import pprint
 
 wordlist = ['rear']
 # wordlist = ['loft','ground','rear']
+
 
 def convert(s):
  
@@ -24,6 +27,11 @@ def convert(s):
  
     # return string
     return new
+
+def format_address(addresss):
+    formatted_address = addresss.replace('\n', ' ')
+    address_list.append(formatted_address)
+
 
 words = convert(wordlist)
 words_search_for = words.rstrip(words[-1])
@@ -47,39 +55,23 @@ input_element1.send_keys('01/01/2024')
 input_element2.send_keys('12/01/2024')
 # Click the search button
 search_element = driver.find_element(By.CLASS_NAME, 'btn-primary')
+
 # Wait for the cookie consent popup to appear
 wait = WebDriverWait(driver, 10)
 cookie_popup = wait.until(EC.presence_of_element_located((By.ID, 'ccc-end')))
 
 # Locate and click the "Accept" button
-print(cookie_popup)
-time.sleep(20)
 accept_button = cookie_popup.find_element(By.ID, 'ccc-dismiss-button')
 accept_button.click()
-time.sleep(4)
-
-  
 
 
+# Click submit for advanced results page
 search_element.click()
-try:
-    # Wait for the cookie consent popup to appear
-    wait = WebDriverWait(driver, 10)
-    cookie_popup = wait.until(EC.presence_of_element_located((By.ID, 'cookie-popup')))
 
-    # Locate and click the "Accept" button
-    accept_button = cookie_popup.find_element(By.ID, 'accept-cookies')
-    accept_button.click()
-
-except TimeoutException:
-    # Cookie consent popup not found, proceed with your code
-    pass
-# Wait for the page to load (you may need to adjust the waiting time)
 wait = WebDriverWait(driver, 10)
 wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'infocontent')))
 driver.execute_script("location.reload(true);")
 
-time.sleep(10)
 
 # Get the page source after the search
 page_source = driver.page_source
@@ -87,8 +79,8 @@ page_source = driver.page_source
 # Parse HTML with BeautifulSoup
 soup = BeautifulSoup(page_source, 'html.parser')
 
-searchResultsPage = soup.find('div', class_='col-a')
-searchResults = searchResultsPage.find_all('li', class_='searchresult')
+searchResultsPage = soup.find('ul', class_='planning-apps')
+searchResults = searchResultsPage.find_all('li')
 
 for row in searchResults:
 
@@ -99,9 +91,12 @@ for row in searchResults:
 for row in row_list:
 
     # Find the address and add to address_list
-    address_div = row.find('p', class_='address')
+    address_div = row.find('h3')
     address = address_div.text.strip()
-    address_list.append(address)
+    format_address(address)
+    
+    
+    
 
     a_tag = row.find('a')
     link_text = a_tag.get_text(strip=True)
@@ -109,28 +104,23 @@ for row in row_list:
 
     element.click()
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located((By.ID, 'subtab_details')))
+    wait.until(EC.presence_of_element_located((By.ID, 'ctl00_PageContent_btnShowApplicantDetails')))
 
-    subtab = driver.find_element(By.ID, 'subtab_details')
+    subtab = driver.find_element(By.ID, 'ctl00_PageContent_btnShowApplicantDetails')
     subtab.click()
 
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'row0')))
+    wait.until(EC.presence_of_element_located((By.ID, 'ctl00_PageContent_lbl_Applic_Name')))
     name_page_source = driver.page_source
     name_soup = BeautifulSoup(name_page_source, 'html.parser')
 
-    name_rows = name_soup.find_all('tr', class_='row0')
-    if len(name_rows) >= 3:
-        name_row = name_rows[2]
-        print(name_row)
-    else: 
-        name_list.append('ERROR ------ STOP')
+
+    name = name_soup.find('span', id='ctl00_PageContent_lbl_Applic_Name')
     
-    name = name_row.find('td')
+    # name = name_rows.find('td')
     name_list.append(name.text.strip())
     driver.back()
     driver.back()
-    driver.execute_script("location.reload(true);")
 
 
 merge_data = zip(name_list, address_list)
