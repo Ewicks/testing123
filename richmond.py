39a54d0df0049c2c3e7da7ccdf41ab7727afe5cb
ethan <ethanowicks9@gmail.com>
2024-01-16 16:20:46 +0000

    Add same fix for richmond

--- richmond.py
+++ richmond.py
@@ -116,10 +116,10 @@ for row in row_list:
     format_address(address)
 
     a_tag = row.find('a')
-    link_text = a_tag.get_text(strip=True)
+    href_value = a_tag.get('href')
     element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, link_text))
-        )
+    EC.element_to_be_clickable((By.XPATH, f"//a[@href='{href_value}']"))
+    )
     element.click()
     try:
         subtab = None
